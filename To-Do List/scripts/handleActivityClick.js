8bec74608db574e988d47a323c57f6e64943ec39
aty101 <atyaty003@gmail.com>
2025-12-26 16:41:10 +0200

    vidoe done

--- To-Do List/scripts/handleActivityClick.js
+++ To-Do List/scripts/handleActivityClick.js
@@ -1,12 +1,20 @@
 const handleActivityClick = (event) => {
   const clickedElement = event.target;
+  console.log(clickedElement.classList)
   if (clickedElement.classList[1] === "fa-trash") {
-    const listItem = target.parentElement.parentElement;
+    const listItem = clickedElement.parentElement.parentElement;
+    
     listItem.remove();
   }
   if (clickedElement.tagName.toLowerCase() === "span") {
     const title = clickedElement.innerHTML;
-    console.log(title);
+    clickedElement.innerHTML = `<input class = "edit-task type="text value="${title}"/>`;
+
+    const editInput = clickedElement.querySelector("input")
+    editInput.addEventListener("blur",()=>{
+      const newVal = editInput.value
+      clickedElement.innerHTML = newVal
+    })
   }
 };
 
